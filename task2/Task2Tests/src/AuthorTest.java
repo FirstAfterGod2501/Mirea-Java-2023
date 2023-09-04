e7d853edf3e9e1e2342ae094dcb4ba7779b99f5a
first <f@a>
2023-09-04 16:51:18 +0300

    add task2

---
+++ task2/Task2Tests/src/AuthorTest.java
@@ -0,0 +1,37 @@
+public class AuthorTest {
+    public AuthorTest() {
+        System.out.println("Author test");
+
+        Author a1 = new Author("Gleb", "gleb@author.com", 'm');
+        Author a2 = new Author("Anastasia", "nastya@author.com", 'w');
+
+        System.out.println("author name test");
+        if(!(a1.GetName() == "Gleb" && a2.GetName() == "Anastasia")){
+            System.out.println("test not passed");
+        }
+        System.out.println("test passed");
+
+        System.out.println("author email test");
+
+        var email = "authors@author.com";
+
+        a1.SetEmail(email);
+        a2.SetEmail(email);
+
+        if(!(a1.GetEmail() == a2.GetEmail() && a2.GetEmail() == email)){
+            System.out.println("test not passed");
+        }
+        System.out.println("test passed");
+
+        System.out.println("author gender test");
+
+        if(!(a1.getGender() == 'm' && a2.getGender() == 'w')){
+            System.out.println("test not passed");
+        }
+        System.out.println("test passed");
+
+        System.out.println(a1);
+        System.out.println(a2);
+
+    }
+}
---
+++ task2/src/Author.java
@@ -0,0 +1,33 @@
+public class Author {
+    private String Name;
+
+    private String Email;
+
+    private char Gender;
+
+    public Author(String name, String email, char gender){
+        Name = name;
+        Email = email;
+        Gender = gender;
+    }
+
+    public String GetName(){
+        return Name;
+    }
+
+    public String GetEmail(){
+        return Email;
+    }
+
+    public void SetEmail(String email){
+        Email = email;
+    }
+
+    public char getGender(){
+        return Gender;
+    }
+
+    public String toString(){
+        return "Name: " + Name + "\nEmail: " + Email + "\nGender: " + Gender;
+    }
+}
