a9f781a5936ca8a2dff3b03c98c99941d718389c
first <f@a>
2023-09-05 13:50:09 +0300

    add task7

---
+++ Task7/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Game("13579", "24680").play());
+        System.out.println(new Game("12345", "67890").play());
+        System.out.println(new Game("13029", "42685").play());
+    }
+}
